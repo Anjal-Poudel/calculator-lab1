974782e88992a6f18a887cc9bb71afbcf1c625bd
anjal <anjalpoudel@gmail.com>
2025-02-01 12:21:37 +0530

    define the Main class and main() method in Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UserInput userInput = new UserInput(scanner);
+        Calculator calculator = new Calculator();
+    }
+}

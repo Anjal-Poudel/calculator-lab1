c503dd58fbe68383c002fbabfdfd01e4f645ff8f
anjal <anjalpoudel@gmail.com>
2025-02-01 12:12:14 +0530

    define the method getArrayInput in UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -19,4 +19,15 @@ public class UserInput {
         int num2 = scanner.nextInt();
         return new int[]{num1, num2};
     }
+
+    public int[] getArrayInput() {
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
 }
